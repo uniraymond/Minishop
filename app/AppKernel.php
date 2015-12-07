c9a48b761f154f6d15b97f734ca7df04ee98f391
Raymond <uniraymond@gmail.com>
2015-12-07 13:13:29 +1300

    #ticket 0006 add Sonata to project

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new Minishop\UserBundle\MinishopUserBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Minishop\BackendBundle\MinishopBackendBundle(),
+            new Sonata\UserBundle\SonataUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
