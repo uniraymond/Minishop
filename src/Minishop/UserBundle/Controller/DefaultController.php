82bfd3d5056ceb26fd76fa831e5094c47264713e
Raymond <uniraymond@gmail.com>
2016-01-01 00:38:42 +1300

    add bundles

--- src/Minishop/UserBundle/Controller/DefaultController.php
+++ src/Minishop/UserBundle/Controller/DefaultController.php
@@ -6,8 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('MinishopUserBundle:Default:index.html.twig', array('name' => $name));
+//        return $this->render('MinishopUserBundle:Default:index.html.twig');
+        return array();
     }
 }
